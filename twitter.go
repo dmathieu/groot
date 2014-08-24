08e01443829307aca5a2853368594581687d94af
Damien Mathieu <damien@heroku.com>
2014-08-24 14:37:49 +0200

    track the number of mentions in the past 24 hours

--- twitter.go
+++ twitter.go
@@ -5,6 +5,7 @@ import (
 	"github.com/ChimeraCoder/anaconda"
 	"net/url"
 	"os"
+  "time"
 )
 
 type TwitterAccount struct {
@@ -38,9 +39,28 @@ func (t *Twitter) fetchUser(account string, collector *Metrics) {
 	if err != nil {
 		Log("Couldn't fetch twitter data")
 	} else {
-		Log("account.data name=%s followers=%d following=%d", user.Name, user.FollowersCount, user.FriendsCount)
+		Log("account.data name=%s followers=%d following=%d", account, user.FollowersCount, user.FriendsCount)
 
-		go collector.TrackGauge(fmt.Sprintf("twitter.%s.followers", account), int64(user.FollowersCount))
-		go collector.TrackGauge(fmt.Sprintf("twitter.%s.following", account), int64(user.FriendsCount))
+    go collector.TrackGauge(fmt.Sprintf("twitter.%s.followers", account), int64(user.FollowersCount))
+    go collector.TrackGauge(fmt.Sprintf("twitter.%s.following", account), int64(user.FriendsCount))
 	}
+
+  searchParams, _ := url.ParseQuery("count=100")
+  search, err := t.Client.GetSearch(fmt.Sprintf("@%s", account), searchParams)
+  if err != nil {
+    Log("Couldn't fetch twitter search data")
+  } else {
+    yesterday := time.Now().Add(-(24 * time.Hour))
+    searchCount := 0
+
+    for _, result := range search {
+      createdAt, _ := result.CreatedAtTime()
+      if createdAt.After(yesterday) {
+        searchCount += 1
+      }
+    }
+
+    Log("account.search name=%s results=%d total=%d", account, searchCount, len(search))
+    go collector.TrackGauge(fmt.Sprintf("twitter.%s.mentions", account), int64(searchCount))
+  }
 }
